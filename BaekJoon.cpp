2749798ebfa81b1a1eba7c4b7ec277be14c0dbdc
unm7925 <unm7925@gmail.com>
2024-05-29 20:53:57 +0900

    >>add 11718

--- BaekJoon.cpp
+++ BaekJoon.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
     string a;
-    int i;
 
-    cin >> a;
-    cin >> i;
-
-    cout << a[i - 1];
+    while (getline(cin, a))
+    {
+        cout << a << "\n";
+    }
 
     return 0;
 }
