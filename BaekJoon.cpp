46ef16d3b172b3dbd7fd2f3b910e302e81b3af10
unm7925 <unm7925@gmail.com>
2024-06-04 22:18:24 +0900

    10818

--- BaekJoon.cpp
+++ BaekJoon.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    int a;
+    // N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+    // 첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다.
+    // 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다.
+    // 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+    // 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
 
-    int total = 0;
+    int n;
 
-    cin >> a;
+    cin >> n;
 
-    char *b = new char[a];
+    long long *number = new long long(n);
 
-    cin >> b;
-
-    for (int i = 0; i < a; i++)
+    for (int i = 0; i < n; i++)
     {
-        total += (b[i] - '0');
+        cin >> number[i];
     }
 
-    cout << total;
+    sort(number, number + n);
 
+    cout << number[0] << " " << number[n - 1];
     return 0;
 }
