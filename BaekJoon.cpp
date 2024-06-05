e10216cd0208c1f564d85e8eb329a5b0944d0194
unm7925 <unm7925@gmail.com>
2024-06-05 14:58:19 +0900

    2439

--- BaekJoon.cpp
+++ BaekJoon.cpp
@@ -5,33 +5,22 @@ using namespace std;
 int main()
 {
 
-    // 문자열 S를 입력받은 후에,
-    // 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오.
-    // 즉, 첫 번째 문자를 R번 반복하고,
-    // 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다.
-    // S에는 QR Code "alphanumeric" 문자만 들어있다.
-    // 문자열 S가 공백으로 구분되어 주어진다.
-    // S의 길이는 적어도 1이며, 20글자를 넘지 않는다.
+    // 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+    // 하지만, 오른쪽을 기준으로 정렬한 별(예제 참고) 을 출력하시오.
+
+    // 입력
+    //     첫째 줄에 N(1 ≤ N ≤ 100) 이 주어진다.int n;
 
     int n;
-
     cin >> n;
-
-    int count;
-    string text;
 
     for (int i = 0; i < n; i++)
     {
-        cin >> count >> text;
+        cout << " ";
 
-        int length = text.length();
-
-        for (int j = 0; j < length; j++)
+        for (int j = 0; j < i; j++)
         {
-            for (int k = 0; k < count; k++)
-            {
-                cout << text[j] << "\n";
-            }
+            cout << "*" << "\n";
         }
     }
 
