b9e2a3373820bbc9bc53f40276209863d6a4e217
unm7925 <unm7925@gmail.com>
2024-06-05 14:02:06 +0900

    2675

--- BaekJoon.cpp
+++ BaekJoon.cpp
@@ -5,25 +5,35 @@ using namespace std;
 int main()
 {
 
-    int n = 9;
-    int max = 0;
-    int tryC = 0;
+    // 문자열 S를 입력받은 후에,
+    // 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오.
+    // 즉, 첫 번째 문자를 R번 반복하고,
+    // 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다.
+    // S에는 QR Code "alphanumeric" 문자만 들어있다.
+    // 문자열 S가 공백으로 구분되어 주어진다.
+    // S의 길이는 적어도 1이며, 20글자를 넘지 않는다.
 
-    int inputN;
+    int n;
+
+    cin >> n;
+
+    int count;
+    string text;
 
     for (int i = 0; i < n; i++)
     {
-        cin >> inputN;
+        cin >> count >> text;
 
-        if (max < inputN)
+        int length = text.length();
+
+        for (int j = 0; j < length; j++)
         {
-            max = inputN;
-            tryC = i;
+            for (int k = 0; k < count; k++)
+            {
+                cout << text[j] << "\n";
+            }
         }
     }
-
-    cout << max << "\n"
-         << tryC + 1;
 
     return 0;
 }
