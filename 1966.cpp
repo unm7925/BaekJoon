419ddce64bf3dfe28479a6e27ea4896f50f9fb70
LeeJiHoon <unm7925@gmail.com>
2024-11-21 17:35:53 +0900

    setting

---
+++ 1966.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+
+    int t;
+
+    cin >> t;
+
+    while (t--)
+    {
+        priority_queue<int> pq;
+        queue<pair<int, int>> que;
+    }
+    return 0;
+}
