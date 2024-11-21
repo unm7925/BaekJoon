fae0312b633319f700f00f931114460c5c8a53e7
unm7925 <79829083+unm7925@users.noreply.github.com>
2024-11-21 17:36:53 +0900

    Update << 1966.cpp

--- 1966.cpp
+++ 1966.cpp
@@ -1,20 +1,59 @@
 #include <iostream>
+#include <vector>
 #include <queue>
 #include <utility>
+#include <algorithm>
 
 using namespace std;
 
+
 int main()
 {
-
-    int t;
-
+    int t; 
     cin >> t;
 
     while (t--)
     {
+        queue<pair<int,int>> que;
         priority_queue<int> pq;
-        queue<pair<int, int>> que;
+
+        int count, sequnce;
+        int print = 1;
+
+        cin >> count >> sequnce;
+
+        for (int i = 0; i < count; i++)
+        {
+            int que_in;
+            cin >> que_in;
+            que.push({ que_in,i });
+            pq.push(que_in);
+        }
+
+        while(true)
+        {
+            int queF = que.front().first;
+            int queS = que.front().second;
+            if (queF != pq.top())
+            {
+                que.pop();
+                que.push({ queF, queS });
+            }
+            else
+            {
+                if (queS == sequnce)
+                {
+                    cout << print << " ";
+                    break;
+                }
+                else
+                {
+                    que.pop();
+                    pq.pop();
+                    print++;
+                }
+            }
+        }
     }
     return 0;
 }
