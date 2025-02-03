71479a22a84b33529eac7898fde0e984128e67f0
unm7925 <unm7925@gmail.com>
2025-02-03 23:37:10 +0900

    Add << 28702.cpp

--- 28702.cpp
+++ 28702.cpp
@@ -7,8 +7,38 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int t;
-    cin >> t;
+    int number = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        string n;
+        // Fizz
+        // Buzz
+
+        cin >> n;
+
+        if (n != "Fizz" && n != "Buzz" && n != "FizzBuzz")
+        {
+
+            number = stoi(n);
+        }
+        number++;
+    }
+
+    if (number % 3 == 0 && number % 5 == 0)
+    {
+        cout << "FizzBuzz";
+    }
+    else if (number % 3 == 0)
+    {
+        cout << "Fizz";
+    }
+    else if (number % 5 == 0)
+    {
+        cout << "Buzz";
+    }
+    else
+        cout << number;
 
     return 0;
 }
